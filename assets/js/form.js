2723fda84b97a5aaf5a13de0a3c6b298666fc178
ksukhalizka <34743651+ksukhalizka@users.noreply.github.com>
2017-12-24 18:57:56 +0300

    1

--- assets/js/form.js
+++ assets/js/form.js
@@ -6,14 +6,14 @@ myform.submit(function(event){
   var service_id = "default_service";
   var template_id = "feedback";
 
-  myform.find("button").text("Отправка...");
+  myform.find("button").text("Sending...");
   emailjs.sendForm(service_id,template_id,"myform")
   	.then(function(){ 
-    	alert("Отправлено!");
+    	alert("Thank you for your message");
        myform.find("button").text("Отправить");
     }, function(err) {
-       alert("Не удалость отправить сообщение!\r\n Ошибка:\n " + JSON.stringify(err));
-       myform.find("button").text("Отправить еще раз");
+       alert("Could't send letter\r\n Error:\n " + JSON.stringify(err));
+       myform.find("button").text("Try again");
     });
   return false;
 });
